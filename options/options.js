ee4cfecab15c34d09e80ee409f76471fd0cfbf14
Crowdbotics <team@crowdbotics.com>
2024-02-05 23:40:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "simplecloudmigratio-46021",
+  url: "https://simplecloudmigratio-46021.botics.co",
+  api: "https://simplecloudmigratio-46021.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
